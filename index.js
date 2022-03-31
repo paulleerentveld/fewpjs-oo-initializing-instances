a320fa84e43abb65399bd9210310f0075f92475b
paulleerentveld <paul@veldnet.com.au>
2022-03-31 13:08:17 +1000

    added classes for lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your code here
+class dog {
+    constructor(name,breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+};
+
+let jackodog= new dog("Jacko", "Bitsa");
+
+jackodog;
+
+
+class Breakfast {
+    constructor(food,drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+};
+
+class Lunch {
+    constructor(salad,soup,drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+};
+
+class Dinner {
+    constructor(salad,soup,entree,dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
+
+let test = new Dinner('greens','potato','crab','cake');
+
+console.log(test._dessert);
